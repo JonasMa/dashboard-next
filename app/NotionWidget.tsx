d4afe248ff40d4d9faf2f99766d05e172011a3a8
Jonas Mattes <github@jmattes.de>
2024-11-06 21:39:23 +0100

    Mark notion widget as unused

--- app/NotionWidget.tsx
+++ app/NotionWidget.tsx
@@ -1,15 +1,24 @@
 import { getFirstNotionTable } from './lib/api';
 import NotionTable from './NotionTable';
+import TaskList from './TaskList';
 
-const NotionWidget = async () => {
-  const notionTable = await getFirstNotionTable();
+// Currently not used
+// @deprecated
+export default async function NotionWidget() {
+  const { tableData, taskList } = await getFirstNotionTable();
 
   return (
     <div className="widget notion-widget">
-      <h2>Wer kocht?</h2>
-      <NotionTable notionTable={notionTable} />
+      <div className="grid grid-cols-2 gap-8">
+        <div>
+          <h2 className="mb-4">Wer kocht?</h2>
+          <NotionTable notionTable={tableData} />
+        </div>
+        <div>
+          <h2 className="mb-4">Wochenaufgaben</h2>
+          <TaskList tasks={taskList} />
+        </div>
+      </div>
     </div>
   );
 };
-
-export default NotionWidget;
